6d75109e57354a5cb0bacfbb9f6ba6f2347e5fc8
daridev <darideveloper@gmail.com>
2025-03-26 15:49:40 -0600

    feat (CategoryButtons): render data from api

---
+++ lib/api.ts
@@ -0,0 +1,28 @@
+import { fetch, FetchRequestInit } from 'expo/fetch';
+import { getAccessToken } from 'store/tokens';
+
+/**
+ * Fetches data from the API using the stored access token
+ *
+ * @param {string} endpoint - The API endpoint to fetch data from
+ * @returns {Promise<void>} Nothing
+ */
+export async function fetchData(endpoint: string): Promise<object[]> {
+  try {
+    const myHeaders = new Headers();
+    const accessToken = await getAccessToken()
+    myHeaders.append('Authorization', `Bearer ${accessToken || ''}`);
+
+    const requestOptions: FetchRequestInit = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+
+    const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE}/${endpoint}/`, requestOptions);
+    const jsonData = await response.json();
+    const results = jsonData.results;
+    return results
+  } catch (error) {
+    return []
+  }
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import { fetch, FetchRequestInit } from 'expo/fetch';
-import { setAccessToken, setRefreshToken, getAccessToken, getRefreshToken } from 'store/tokens';
+import { setAccessToken, setRefreshToken } from 'store/tokens';
 
 interface TokenData {
   refresh: string;
@@ -13,36 +13,39 @@ interface ApiResponse {
 }
 
 /**
- * Login with guest user and password* 
-*/
+ * Login with guest user and password*
+ * 
+ * @returns {Promise<ApiResponse | { status: number }>}
+ */
 export async function loginGuest(): Promise<ApiResponse | { status: number }> {
   const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append('Content-Type', 'application/json');
 
   const raw = JSON.stringify({
-    "username": process.env.EXPO_PUBLIC_API_GUEST_USER,
-    "password": process.env.EXPO_PUBLIC_API_GUEST_PASS,
+    username: process.env.EXPO_PUBLIC_API_GUEST_USER,
+    password: process.env.EXPO_PUBLIC_API_GUEST_PASS,
   });
 
   const requestOptions: FetchRequestInit = {
-    method: "POST",
+    method: 'POST',
     headers: myHeaders,
     body: raw,
   };
 
-  // Get tokens dnd validate response 
-  const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE}/token/`, requestOptions)
+  // Get tokens dnd validate response
+  const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE}/token/`, requestOptions);
   if (response.status !== 200) {
-    return { status: response.status }
+    return { status: response.status };
   }
 
   // Get tokens
-  const jsonData = await response.json()
+  const jsonData = await response.json();
 
   // Update tokens
-  setAccessToken(jsonData.data.access)
-  setRefreshToken(jsonData.data.refresh)
+  setAccessToken(jsonData.data.access);
+  setRefreshToken(jsonData.data.refresh);
 
   // Return data
-  return jsonData as ApiResponse
+  return jsonData as ApiResponse;
 }
+
